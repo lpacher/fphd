f72b0f6e0d6a52a7add6b8849d589335231136ca
Luca Pacher <pacher@to.infn.it>
2025-06-03 16:06:04 +0200

    Removed legacy sources, will be added again later on

--- fpga/labs/lab5/.solutions/scripts/debug/add_ila.tcl
+++
@@ -1,5 +0,0 @@
-##
-## Use this script to select RTL signals to be monitored on real hardware
-## using the Xilinx Vivado Integrated Logic Analyzer (ILA) tool.
-##
-
--- fpga/labs/lab6/README.md
+++
@@ -1,3 +0,0 @@
-# Lab 6 Instructions
-[[**Home**](https://github.com/lpacher/lae)] [[**Back**](https://github.com/lpacher/fphd/tree/master/fpga/labs)]
-
